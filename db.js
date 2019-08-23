350ce563e3ee4005309e1761e8f6c205987c256f
Tu Do Van <tudv@UNIT.COM>
2019-08-23 18:28:23 +0700

    fix bugggg

--- db.js
+++ db.js
@@ -13,29 +13,17 @@ const db = new sequelize({
     define: { // not add 's' to model name
         freezeTableName: true
     }
-
 })
 
 // db.authenticate()
 //     .then(() => console.log('>>>>>> ket noi thanh cong'))
 //     .catch(err => console.log(err.message))
 
-
-
 // const roomlists = db.define('roomlists',{
 //     room_id: sequelize.STRING,
 //     room_name: sequelize.STRING,
 //     user_id: sequelize.STRING,
 
-
-// })
-
-// const messages = db.define('messages',{
-//     createdAt: sequelize.STRING,
-//     id: sequelize.STRING,
-//     is_read: sequelize.STRING,
-//     text: sequelize.STRING,
-//     user_id: sequelize.STRING
 // })
 
 const user = db.define('user', { // model : table in database
@@ -46,16 +34,9 @@ const user = db.define('user', { // model : table in database
     sub_id: sequelize.STRING,
     user_id: sequelize.STRING,
 })
-
-// db.sync() // tao model
-//     .then(() => console.log('>>>>>> tao thanh cong'))
-
-
-// update database 
-
 // user.create({
 //     id_user: '123',
-//     avatar: 'abc',
+//     avatar: 'https://placeimg.com/140/140/any',
 //     emai: 'dovantuit@gmail.com',
 //     name: 'do van tu',
 //     sub_id: '70965456',
@@ -63,24 +44,48 @@ const user = db.define('user', { // model : table in database
 // }).then(user => console.log(user.get({ plain: true })))
 // .catch(err =>console.log(err.message))
 
-// roomlist.create({
-//     room_id: '1234',
-//     room_name: 'zalo',
-//     user_id: '123',
-// }).then(user=>console.log(user.get({plain: true})))
+const roomlists = db.define('roomlists', { // model : table in database
+    room_id: sequelize.STRING,
+    room_name: sequelize.STRING,
+    user_id: sequelize.STRING,
+    id_guess: sequelize.STRING,
+    id_owner: sequelize.STRING,
+})
+// roomlists.create({
+//     room_id: '123',
+//     room_name: 'phong 1',
+//     user_id: 'dovantuit@gmail.com',
+//     id_guess: '70965456235432',
+//     id_owner: '70965456',
+// }).then(roomlists => console.log(roomlists.get({ plain: true })))
+// .catch(err =>console.log(err.message))
 
+const messages = db.define('messages', {
+    room_id: sequelize.STRING,
+    message_id: sequelize.STRING,
+    text: sequelize.STRING,
+    user_id: sequelize.STRING,
+    user_name: sequelize.STRING,
+})
+// messages.create({
+//     room_id: '123',
+//     message_id: '1232',
+//     text: 'dsafd ',
+//     user_id: '11231',
+//     user_name: 'do van tom',
+// }).then(messages => console.log(messages.get({ plain: true })))
+// .catch(err =>console.log(err.message))
 
-
+// db.sync() // tao model
+//     .then(() => console.log('>>>>>> tao thanh cong'))
+// update database 
 // tạo nhiều dòn trong table
 // user.bulkCreate([
 //     {username:"sdsf",password:"123"},
 //     {username:"tsasadfdfin",password:"123"}
-
 // ]).then(arr_user=>arr_user.forEach(user =>console.log(user.get({plain: true}))))
 
-
 // xóa table
-
 // user.destroy({
 //     where:{
 //         id:4
@@ -107,12 +112,9 @@ const user = db.define('user', { // model : table in database
 // user.findAll({raw: true})
 // .then(arr_user=>arr_user.forEach(user=>console.log(user)))
 
-
 // findById
 // user.findById(5,{raw: true})
 // .then(user=>console.log(user))
-
-
 
 // user.findAll({
 //     where: {
@@ -123,7 +125,4 @@ const user = db.define('user', { // model : table in database
 //     .then(users =>console.log(users))
 //     .catch(err => console.log(err.message))
 
-
-
-
-    module.exports = user
+module.exports = { user, messages, roomlists }
--- index.js
+++ index.js
@@ -2,34 +2,32 @@
 var express = require("express");
 var app = express();
 const bodyParser = require('body-parser');
-const user = require('./db.js')
-
-
+const { user, messages, roomlists } = require('./db.js')
 // app.use(express.static("public"));
 // app.set("view engine", "ejs");
 // app.set("views","./views")
 
-
-
-
 var server = require("http").Server(app);
 server.listen(3000, () => console.log('server chay port 3000'));
-
 app.use(bodyParser.urlencoded({ extended: true }))
 // app.get("/",function(req,res){
 //     res.render("trangchu");
 // });
 app.get("/", (req, res) => res.send('xin chao'));
 
+app.get('/user_read', (req, res) => {
+    user.findAll()
+        .then((users) => res.json({ 'kq': 1, 'user': users }))
+        .catch(err => res.json({ 'kq': 0 }))
+})
 
-app.post('/add', (req, res) => {
-    const id_user = req.body.id_user
-    const avatar = req.body.avatar
-    const emai = req.body.emai
-    const name = req.body.name
-    const sub_id = req.body.sub_id
-    const user_id = req.body.user_id
-
+app.post('/user_add', (req, res) => {
+    let id_user = req.body.id_user
+    let avatar = req.body.avatar
+    let emai = req.body.emai
+    let name = req.body.name
+    let sub_id = req.body.sub_id
+    let user_id = req.body.user_id
     user.create({
         id_user: id_user,
         avatar: avatar,
@@ -42,27 +40,27 @@ app.post('/add', (req, res) => {
         .catch(err => res.json({ 'kq': 0 }))
 })
 
-app.get('/read', (req, res) => {
-    user.findAll()
-        .then((users) => res.json({ 'kq': 1, 'user': users }))
-        .catch(err => res.json({ 'kq': 0 }))
-})
-
-app.post('/update', (req, res) => {
-    const { id,
-        id_user,
-        avatar,
-        emai,
-        name,
-        sub_id,
-        user_id } = req.body
+app.post('/user_update', (req, res) => {
+    // const { id,
+    //     // id_user,
+    //     //avatar,
+    //     emai,
+    //     //name,
+    //     //sub_id,
+    //     //user_id } = req.body
+    console.log(req.body);
+    // console.log(req.body);
+    let emai = req.body.emai;
+    console.log(emai);
+    let id = req.body.id
     user.update({
-        id_user: id_user,
-        avatar: avatar,
-        emai: emai,
-        name: name,
-        sub_id: sub_id,
-        user_id: user_id
+        //id: id,
+        ///id_user: id_user,
+        //avatar: avatar,
+        emai: emai
+        //name: name,
+        //sub_id: sub_id,
+        //user_id: user_id
 
     }, {
             where: { id: id }
@@ -71,11 +69,44 @@ app.post('/update', (req, res) => {
         .catch(err => res.json({ 'kq': 0 }))
 })
 
-app.post('/delete', (req, res) => {
+app.post('/user_delete', (req, res) => {
     const { id } = req.body
     user.destroy({
         where: { id: id } //id thu 2 la id lay tu req.body
     })
         .then((row) => res.json({ 'kq': 1, 'row': row }))
+        .catch(err => res.json({ 'kq': 0 }))
+})
+
+// roomlists
+app.get('/roomlists_read', (req, res) => {
+    roomlists.findAll()
+        .then((roomlists) => res.json({ 'kq': 1, 'roomlists': roomlists }))
+        .catch(err => res.json({ 'kq': 0 }))
+})
+
+app.post('/roomlists_add', (req, res) => {
+    const room_id = req.body.room_id
+    const room_name = req.body.room_name
+    const user_id = req.body.user_id
+    const id_guess = req.body.id_guess
+    const id_owner = req.body.id_owner
+
+    roomlists.create({
+        room_id: room_id,
+        room_name: room_name,
+        user_id: user_id,
+        id_guess: id_guess,
+        id_owner: id_owner,
+
+    }).then(() => res.json({ 'kq': 1 }))
+        .catch(err => res.json({ 'kq': 0 }))
+})
+
+
+// messages
+app.get('/messages_read', (req, res) => {
+    messages.findAll()
+        .then((messages) => res.json({ 'kq': 1, 'messages': messages }))
         .catch(err => res.json({ 'kq': 0 }))
 })
